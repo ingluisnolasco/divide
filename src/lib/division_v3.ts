74a12cdc550dbc9fb5abbe4119c51e862f6efa13
ingluisnolasco <ing.luisnolasco@gmail.com>
2024-09-16 20:59:57 -0400

    Update division_v3.ts

--- src/lib/division_v3.ts
+++ src/lib/division_v3.ts
@@ -68,7 +68,7 @@ export function division(dividendo: string, divisor: string, decimales: string):
 
     if (decimales === "0") {
         cociente = (format(divide(bignumber(dividendo), bignumber(divisor)),
-            { notation: 'fixed', precision: 0 }));
+            { notation: 'fixed', precision: 1 })).slice(0, -1);
 
     }
     else {
